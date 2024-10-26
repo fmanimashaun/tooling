37abe597c90dbb401a0ce8b6692a9d734e7926c4
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-10-26 23:05:58 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,6 +1,11 @@
 <?php 
 include('functions.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if (!isLoggedIn()) {
     $_SESSION['msg'] = "You must log in first";
     header('location: login.php');
